dfa4a1c8d95e49ffa2e19b423244c409a1713e7e
sarahDLB <dlbsarah93@hotmail.fr>
2019-06-25 13:17:36 +0200

    artcont

--- projetVcMvc2/src/main/java/controller/ArticleController.java
+++ projetVcMvc2/src/main/java/controller/ArticleController.java
@@ -5,8 +5,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
-import formationJpaSpring.repository.SoldatRepository;
+import videoClub.model.Article;
+import videoClub.model.BluRay;
+import videoClub.model.Dvd;
+import videoClub.repository.ArticleRepository;
+
 
 @Controller
 @RequestMapping ("/article")//
@@ -16,9 +22,28 @@ public class ArticleController {
 	private ArticleRepository articleRepository;
 	
 	@GetMapping("/list")
-	public String list(Model model) {
+	public ModelAndView list() {//gere model et string en meme temps
 		
-		model.addAttribute("listeArticle", articleRepository.findAll());
-		return "article/list";
+		return new ModelAndView ("article/list", "listeArticle", articleRepository.findAll());//la ou il doit aller, nom de ce qu'on affiche, ce quon affiche
+	
+	}
+	
+	@GetMapping("/delete")
+	public String delete(@RequestParam(name="id") int id, Model model) {
+		articleRepository.deleteById(id);
+		return "redirect:/article/list";
+
+	}
+	
+	@GetMapping("/addDvd")
+	public ModelAndView addDvd() {
+		return goEdit(new Dvd());
+	}
+	@GetMapping("/addBluRayt")
+	public ModelAndView addPistolet() {
+		return goEdit(new BluRay());
+	}
+	public ModelAndView goEdit(Article article) {
+		return new ModelAndView("article/edit", "article", article);
 	}
 }
